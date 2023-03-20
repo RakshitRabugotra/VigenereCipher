59c76d8b1d92332e82b0f6e4ff5b2ff7f519a648
Rakshit Rabugotra <90450441+RakshitRabugotra@users.noreply.github.com>
2023-03-20 11:21:10 +0530

    Rectify include statement for header file

--- vigenereCipherTable.cpp
+++ vigenereCipherTable.cpp
@@ -1,51 +1,5 @@
 #include <iostream>
-
-const int LETTERS = 26;
-const int A = 65;
-const int Z = A + LETTERS;
-
-
-/**
- * Shifts the given string by 'shift' characters in right circular manner
- * 
- * @param sample the given string to shift
- * @param shift the numbers to characters to shift
- * @return sample string shifted by 'shift' characters
- */
-std::string circShiftRight(std::string sample, int shift);
-
-/**
- * Shifts the given string by 'shift' characters in left circular manner
- * 
- * @param sample the given string to shift
- * @param shift the numbers to characters to shift
- * @return sample string shifted by 'shift' characters
- */
-std::string circShiftLeft(std::string sample, int shift);
-
-
-/**
- * Generates the Vigenere Cipher Table
- * @return Vigenere Cipher Table
-*/
-char** generateVigenereCipherTable();
-
-/**
- * Prints a 2D array
-*/
-template<typename T>
-void print2DArray(T** array, int rows, int columns);
-
-
-int main(int argc, char** argv) {
-
-    char** vigenereTable = generateVigenereCipherTable();
-
-    // Print the table
-    print2DArray(vigenereTable, LETTERS, LETTERS);
-
-    return 0;
-}
+#include "vigenereCipherTable.hpp"
 
 
 std::string circShiftRight(std::string sample, int shift) {
